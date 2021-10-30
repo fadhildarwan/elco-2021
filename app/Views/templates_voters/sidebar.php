248a61cf56ad15b3065db9f97531e48c5187051d
fadhilanugrahdarwan <fadhilanugrahdarwan@gmail.com>
2021-10-30 21:52:47 +0700

    make it better

--- app/Views/templates_voters/sidebar.php
+++ app/Views/templates_voters/sidebar.php
@@ -1,73 +1,80 @@
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-    <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?=base_url()?>">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-user"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">Elco</div>
-    </a>
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?=base_url()?>">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">ELCO</div>
+            </a>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0">
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
 
-    <!-- Nav Item - Vote -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?=base_url()?>">
-            <i class="fas fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="<?=base_url()?>">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+            <!-- Divider -->
+            <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Voters Access
-    </div>
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Voters Access
+            </div>
 
-    <!-- Nav Item - Vote -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?=base_url('vote')?>">
-            <i class="fas fa-vote-yea"></i>
-            <span>Vote</span></a>
-    </li>
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="<?=base_url('vote')?>" data-toggle="collapse" data-target="#collapseTwo"
+                    aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>Vote</span>
+                </a>
+            </li>
 
-    <!-- Nav Item - Candidate -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?=base_url('candidate')?>">
-            <i class="fas fa-user-tie"></i>
-            <span>Candidate</span></a>
-    </li>
+            <!-- Nav Item - Utilities Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="<?=base_url('candidate')?>" data-toggle="collapse" data-target="#collapseUtilities"
+                    aria-expanded="true" aria-controls="collapseUtilities">
+                    <i class="fas fa-fw fa-wrench"></i>
+                    <span>Candidate</span>
+                </a>
+            </li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+            <!-- Divider -->
+            <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Committee Access
-    </div>
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Admin Access
+            </div>
 
-    <!-- Nav Item - Result -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?=base_url('/admin/result')?>">
-            <i class="fas fa-poll-h"></i>
-            <span>Result</span></a>
-    </li>
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+                    aria-expanded="true" aria-controls="collapsePages">
+                    <i class="fas fa-fw fa-folder"></i>
+                    <span>Result</span>
+                </a>
+            </li>
 
-    <!-- Nav Item - Voters -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?=base_url('/admin/voters')?>">
-            <i class="fas fa-address-book"></i>
-            <span>Voters List</span></a>
-    </li>
+            <!-- Nav Item - Charts -->
+            <li class="nav-item">
+                <a class="nav-link" href="charts.html">
+                    <i class="fas fa-fw fa-chart-area"></i>
+                    <span>Voters List</span></a>
+            </li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
-
-</ul>
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+        </ul>
+        <!-- End of Sidebar -->
