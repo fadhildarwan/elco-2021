ed93f9dad4e73fc88d441a9135be2ef06da1077b
fadhilanugrahdarwan <fadhilanugrahdarwan@gmail.com>
2021-10-30 22:29:55 +0700

    make it better

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,32 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\ResultModel; 
+use App\Models\VotersModel;
 
-class Home extends BaseController
+class Votee extends BaseController
 {
+    protected $result;
+    protected $voters;
+    public function __construct()
+    {
+        $this->result = new ResultModel();
+        $this->voters = new VotersModel();
+
+    }
+
     public function index()
     {
-        return view('welcome_message');
+
+        $data = [
+            'result_id' => $this->result->result_id(), 
+            'result_all' => $this->result->result_all(),  
+            
+            'voters_list' => $this->voters->voters_list(), 
+            
+            'persen' => '100',
+        ];
+    
+        return view('votee', $data);
     }
 }
