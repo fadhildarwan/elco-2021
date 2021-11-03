1b9d56b5dbdacddcca6d2eb96605e1c1a3cc8965
fadhilanugrahdarwan <fadhilanugrahdarwan@gmail.com>
2021-11-03 17:21:59 +0700

    make it better

--- app/Views/admin/result.php
+++ app/Views/admin/result.php
@@ -31,37 +31,6 @@
             <li><?php foreach($result_blank->getResult() as $value): ?>
                 Total Kotak Kosong (<?= $vote=$value->jumlah/$result_id*$persen; $hasil = number_format($vote, 2, '.', ''); echo $hasil; ?>) %
             <?php endforeach ?></li>
-            <li>
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-info shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Paslon 1
-                                    </div>
-                                    <div class="row no-gutters align-items-center">
-                                    <div class="col-auto">
-                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
-                                            <?php foreach($result_paslon->getResult() as $value): ?>
-                                                <?php $vote=$value->jumlah/$result_id*$persen; $output = number_format($vote, 2, '.', ''); echo $output; ?> %
-                                            <?php endforeach ?>
-                                        </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="progress progress-sm mr-2">
-                                            <div class="progress-bar bg-info" role="progressbar" style="width: <?php $vote=$result_paslon/$result_id*$persen; $output = number_format($vote, 2, '.', ''); echo $output; ?>;" aria-valuenow="<?php $vote=$result_paslon/$result_id*$persen; $output = number_format($vote, 2, '.', ''); echo $output; ?>" aria-valuemin="0" aria-valuemax="100"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-spinner fa-2x text-gray-300"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </li>
         </ol>
         <p>Data dihitung berdasarkan jumlah</p>
         <ol>
